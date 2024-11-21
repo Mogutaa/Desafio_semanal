0a8d12bfd5d2eef932e24559f517105020a46168
Alan José <alanjose1d2013@gmail.com>
2024-11-21 11:06:32 -0300

    Pastas para o desafio semanal
    Adicionado as pastas para cada projeto programado

---
+++ 01 - Fácil (Semanas 1 a 12)/01-Calculadora Básica/main.py
@@ -0,0 +1,19 @@
+def soma (a,b):
+    return a+b
+
+def subtracao (a,b):
+    return a-b
+
+def multiplicacao (a,b):
+    return a*b
+
+def divisao (a,b):
+    if b == 0:
+        "Não permitido divisão por zero"
+    return a/b
+
+
+
+
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
